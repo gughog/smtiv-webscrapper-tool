be8a387a783502e677db6a66394d2474634a56ad
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-02-06 17:47:33 -0300

    Finished sql_builder method.

--- src/helpers/sql_builder.js
+++ src/helpers/sql_builder.js
@@ -1,13 +1,36 @@
-const sql_builder = (params) => {
-  let final_sql = `
-    INSERT INTO $1 (
-      ...table_values_here
-    ) VALUES (
-      ...iteration_here
-    );
-  `;
+const fs = require('fs');
+const clr = require('clr-js');
+const OUTPUT_PATH = '../data/';
 
-  return final_sql;
+/**
+ * Creates a dynamic SQL script, based on Postgres dialect. There
+ * is no problem here for concatenating strings, because it will not
+ * execute the script for queries, it only creates the raw script.
+ *
+ * @param {*} filename - Optional file name. Default: 'sql_script';
+ * @param {*} tablename - Name of the table that script will insert into;
+ * @param {*} params - All related data that will be injected.
+ */
+const sql_builder = (filename = 'sql_script', tablename, params) => {
+  let final_sql = `INSERT INTO ${tablename} ( num_index, name ) \nVALUES ${params.indexes.map((el, i) => `("${el}", "${params.names[i]}")`)};`;
+
+  fs.writeFile(`${OUTPUT_PATH}${filename}.sql`, final_sql, 'utf-8', (err) => {
+    if (err) {
+      console.log(clr.red('Error!').bold().it())
+      throw err;
+    }
+
+    console.log(clr.green('File created.').bold().it())
+  })
+
+  // return is needed ?
+  // return final_sql;
 };
+
+// Example usage
+// sql_builder('mysql', 'tablename01', {
+//   indexes: indexes,
+//   names: names
+// });
 
 module.exports = sql_builder;
