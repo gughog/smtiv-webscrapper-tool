efc122919eaa781d82b7012d4d430cd64aec5742
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-02-06 17:05:48 -0300

    Json builder function done.

--- src/helpers/json_builder.js
+++ src/helpers/json_builder.js
@@ -1,14 +1,43 @@
-const json_builder = (params) => {
+const clr = require('clr-js');
+const OUTPUT_PATH = '../data/';
+
+/**
+ * It creates a json file for objects passed
+ * as paramenters.
+ * @param {String} filename - Optional name for file output.
+ * @param {Object} params - The object with all datas.
+ */
+const json_builder = (filename = 'db', params) => {
   let final_json = [];
 
-  params.creatures.forEach(creature => {
+  for (let i = 0; i < params.array.length; i++) {
     final_json.push({
-      key: value
+      index: params.index[i],
+      name: params.array[i]
     })
+  }
+
+  fs.writeFile(`${OUTPUT_PATH}${filename}.json`, JSON.stringify(final_json, null, 2), 'utf-8', (err) => {
+    if (err) {
+      console.log(clr.red('Error!').bold().it())
+      throw err;
+    }
+
+    console.log(clr.green('File created.').bold().it())
   })
 
-  return final_json;
+  // Return will be needed ?
+  // return final_json;
 };
+
+
+////// ===========================================================
+
+// >> Function is called, passing data:
+// json_builder({
+//   index: index,
+//   array: arr1
+// });
 
 module.exports = json_builder;
 
