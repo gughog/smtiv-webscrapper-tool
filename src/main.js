2a7bec712b0ec3cf106e09578a0daa1df2a870c0
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-02-07 14:23:05 -0300

    Finished first structure for this project. Json and SQL generations are working.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,18 @@
 // Constants
-const DATA_PATH = './data';
 const URL = 'https://erikku.github.io/smt4tool/';
 const SHOW_ALL_BTN = '#showAllDemonsBtn';
+const TABLE_ROW_REF = '#demonList > table > tbody > tr';
 
-const TABLE_INFO = Object.freeze({
-  TABLE_ID: '#demonList',
-  TABLE_HEADER: 'table tbody tr th th a', // first index
-  TABLE_DATA: 'table tbody tr' // from index 1 to beyond
-})
-
-// Globals
-let data_array = [];
+const json_builder = require('./helpers/json_builder');
+const sql_builder = require('./helpers/sql_builder');
 
 /**
  * Main function that starts the program.
  */
-const main = async (puppeteer, clr, fs) => {
-  console.log(clr.bold('=== SMTIV Tool ===').red().it());
+const main = async (puppeteer, clr) => {
+  console.log(clr.bold('=== SMTIV Tool (ver. 0.1.0) ===').red().it());
   // 1. Open a new browser:
-  console.log('>>> Opening a browser instance...')
+  console.log(clr.bold('>>> Opening a browser instance...').it())
   const browser = await puppeteer.launch({
     args: ['--no-sandbox'],
     headless: true
@@ -27,32 +21,50 @@ const main = async (puppeteer, clr, fs) => {
     // All logics here:
 
     // 2. Open up a new page:
-    console.log('>>> Opening a new page...')
+    console.log(clr.bold('>>> Opening a new page...').it())
     const page = await browser.newPage();
 
     // 3. Go to URL:
     await page.goto(URL);
 
     // 4. Wait for get all data button press:
-    console.log('>>> Pressing the button...')
+    console.log(clr.bold('>>> Pressing the button...').it())
     await page.click(SHOW_ALL_BTN);
 
-    // 5. Fetch the table fields:
-    console.log('>>> Fetching table info...')
-    const creatures = await page.$$eval('.demonLink', (table) => {
-      return table.map(creature => creature.innerHTML)
+    // 5. Fetch all demons:
+    console.log(clr.bold('>>> Fetching demons...').it())
+    let all_demons = await page.$$eval(TABLE_ROW_REF, rows => {
+      return Object.freeze(rows.map(row => {
+        const table_datas = [...row.querySelectorAll('td')];
+        const array_with_rows = table_datas.map(td => td.textContent);
+        return {
+          lv: array_with_rows[0],
+          name: array_with_rows[1],
+          hp: array_with_rows[2],
+          mp: array_with_rows[3],
+          st: array_with_rows[4],
+          dx: array_with_rows[5],
+          ma: array_with_rows[6],
+          ag: array_with_rows[7],
+          lu: array_with_rows[8],
+          phys: array_with_rows[9],
+          gun: array_with_rows[10],
+          fire: array_with_rows[11],
+          ice: array_with_rows[12],
+          elec: array_with_rows[13],
+          force: array_with_rows[14],
+          light: array_with_rows[15],
+          dark: array_with_rows[16]
+        };
+      }))
     });
 
-    // data_array = json_builder({});
-    console.log(creatures);
-
-    ////////// (fs) instance will be passed to this main fn, so we pass
-    ////////// it to builder methods.
-
+    // console.log(all_demons);
+    await json_builder('demons', all_demons);
+    await sql_builder('insert_demons', 'demons', all_demons)
   } catch (error) {
     throw new Error(error);
   } finally {
-    console.log('>>> Closing the app...')
     await browser.close();
   }
 };
